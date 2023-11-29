967eb549065d43cdf0b5dab97aa09707f2e64bd5
simranjit singh <simranjits008@gmail.com>
2023-11-29 21:01:55 +0530

    update

--- constant.js
+++ constant.js
@@ -1,3 +1,3 @@
 require("dotenv").config();
 
-exports.connectionString = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DATABASE}.0bsrtss.mongodb.net/`;
+exports.connectionString = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DATABASE}/`;
